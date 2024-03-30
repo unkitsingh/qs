182004ba3bb408cf5763aeac115ef06b2b1ca57c
Ankit singh <77711738+unkitsingh@users.noreply.github.com>
2024-03-30 11:56:02 +0530

    Update linkRedirectController.js

--- controllers/linkRedirectController.js
+++ controllers/linkRedirectController.js
@@ -1,5 +1,5 @@
 import Link from "../models/Link.js";
-
+import TelegramShorten from "../models/telegramShortenLink.js";
 let handelRedirectLink = async (req, res) => {
   let id = req.params.id;
   try {
@@ -12,4 +12,17 @@ let handelRedirectLink = async (req, res) => {
     res.send(error.message);
   }
 };
-export { handelRedirectLink };
+let handelTelegramRedirectLink = async (req, res) => {
+  let id = req.params.id;
+  try {
+    let checkId = await TelegramShorten.findOne({ shortLink: id });
+    if (!checkId) return res.json({ status: "fail", message: "invalid link" });
+    let clicks = checkId.totalClicks + 1;
+    await Link.updateOne({ shortLink: id }, { $set: { totalClicks: clicks } });
+    res.redirect(checkId.ogLink);
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+
+export { handelRedirectLink, handelTelegramRedirectLink };
